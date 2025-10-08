bb7c30b79142f9ecc028fa845b525a912bb82ff5
harry1-0 <214026279+harry1-0@users.noreply.github.com>
2025-10-08 03:13:04 -0400

    declared scoure counter variables for players and wrote a function to play a single round.

--- script.js
+++ script.js
@@ -10,34 +10,57 @@ function getComputerChoice() {
 let computerChoice = getComputerChoice();
 console.log("Computer choice: " + computerChoice)
 
- //2: write a function that gets users choice
-        let getHumanChoice = function () {
-            let humanChoice = prompt("What's your choice? Rock or Paper or Scissor");
+//2: write a function that gets users choice
+let getHumanChoice = function () {
+    let humanChoice = prompt("What's your choice? Rock or Paper or Scissor");
 
-            humanChoice = humanChoice.trim();
-            humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
-            // console.log("choice: " + humanChoice);
-            let choice = humanChoice;
-            if (!((choice === "Rock" || choice === "R") || (choice === "Paper" || choice === "P") || (choice === "Scissor" || choice === "S"))) {
-                alert(`please Enter A valid Choice.\n"${choice}" is not a valid option\nRock or Paper or Scissor`);
-                return getHumanChoice();
-            }
-          
-            if (choice === "Rock" || choice === "R") {
-                
-                return "Rock";
-            } else if (choice === "Paper" || choice === "P") {
-                
-                return "Paper";
-            } else if (choice === "Scissor" || choice === "S") {
-               
-                return "Scissor";
-            }
+    humanChoice = humanChoice.trim();
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+    // console.log("choice: " + humanChoice);
+    let choice = humanChoice;
+    if (!((choice === "Rock" || choice === "R") || (choice === "Paper" || choice === "P") || (choice === "Scissor" || choice === "S"))) {
+        alert(`please Enter A valid Choice.\n"${choice}" is not a valid option\nRock or Paper or Scissor`);
+        return getHumanChoice();
+    }
 
-            // console.log("get human ", choice)
-            return getHumanChoice();
-        }
+    if (choice === "Rock" || choice === "R") {
 
-        let humanChoice = getHumanChoice();
-        console.log("Human choice: " + humanChoice);
-        
+        return "Rock";
+    } else if (choice === "Paper" || choice === "P") {
+
+        return "Paper";
+    } else if (choice === "Scissor" || choice === "S") {
+
+        return "Scissor";
+    }
+
+    // console.log("get human ", choice)
+    return getHumanChoice();
+}
+let humanChoice = getHumanChoice();
+console.log("Human choice: " + humanChoice);
+
+
+//3: declare players scores vars and intiallize from 0.
+let humanScore = 0;
+let computerScore = 0;
+
+
+//4: Write the logic to play a single round
+function playRound(human, computer) {
+
+    if ((human === "Rock" && computer === "Rock") || (human === "Paper" && computer === "Paper") || (human === "Scissor" && computer === "Scissor")) {
+        alert(`human: ${human} Vs computer: ${computer}\nIts a tei. Play again!`);
+        console.log(`human: ${human} Vs computer: ${computer}\nIts a tei...🤝🤝🤝 Play again!`);
+    } else if ((computer === "Paper" && human === "Rock") || (computer === "Scissor" && human === "Paper") || (computer === "Rock" && human === "Scissor")) {
+        computerScore++;
+        alert(`🤖Compter Wins!.. ${computer} beats ${human}\nScores ---> Human = ${humanScore} VS computer = ${computerScore}`);
+        console.log(`🤖Compter Wins!..🎉🎉🎉 ${computer} beats ${human}\nScores ---> Human = ${humanScore} VS computer = ${computerScore}`);
+    } else {
+        humanScore++;
+        alert(`👨Human Wins!.. ${human} beats ${computer}\nScores ---> Human = ${humanScore} VS computer = ${computerScore}`);
+        console.log(`👨Human Wins!..🎉🎉🎉 ${human} beats ${computer}\nScores ---> Human = ${humanScore} VS computer = ${computerScore}`);
+    }
+
+}
+playRound(humanChoice, computerChoice);
